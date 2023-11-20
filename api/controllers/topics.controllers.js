1dffb211cf70d28640551659c990bb324fcb852d
HudaifDaif <107198955+HudaifDaif@users.noreply.github.com>
2023-11-20 14:07:59 +0000

    complete TDD for get /api endpoint

--- api/controllers/topics.controllers.js
+++ api/controllers/topics.controllers.js
@@ -1,7 +1,11 @@
-const { selectTopics } = require("../models/topics.models");
+const { selectTopics, listEndpoints } = require("../models/topics.models");
 
 exports.getTopics = (req, res, next) => {
 	selectTopics().then(({ rows }) => {
 		res.status(200).send({ topics: rows });
 	});
+};
+
+exports.getApi = (req, res, next) => {
+	res.status(200).send(listEndpoints());
 };
--- api/models/topics.models.js
+++ api/models/topics.models.js
@@ -1,7 +1,12 @@
 const db = require("../../db/connection");
+const endpoints = require("../../endpoints");
 
 exports.selectTopics = () => {
 	return db.query(`
         SELECT * FROM topics
         ;`);
+};
+
+exports.listEndpoints = () => {
+	return endpoints;
 };
