ad1f98214465665bb5a807513cb6a07e9857a174
HudaifDaif <107198955+HudaifDaif@users.noreply.github.com>
2023-12-11 10:00:40 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,32 +1,20 @@
 const express = require("express");
-const {
-	getCommentsById,
-	deleteCommentById,
-	postComment,
-} = require("./api/controllers/comments.controllers");
-const { getApi } = require("./api/controllers/api.controllers");
-const { getTopics } = require("./api/controllers/topics.controllers");
+const cors = require("cors");
 const {
 	handleBadPath,
 	handlePostgresErrors,
 	handleCustomErrors,
 	handleServerErrors,
 } = require("./api/errors");
-const {
-	patchArticleById,
-	getArticles,
-	getArticleById,
-} = require("./api/controllers/article.controllers");
-const { getUsers } = require("./api/controllers/users.controllers");
 const apiRouter = require("./routes/api-router");
 const articlesRouter = require("./routes/articles-router");
 const usersRouter = require("./routes/users-router");
 const commentsRouter = require("./routes/comments-router");
-const topics = require("./db/data/test-data/topics");
 const topicsRouter = require("./routes/topics-router");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
@@ -45,6 +33,5 @@ app.use(handlePostgresErrors);
 app.use(handleCustomErrors);
 
 app.use(handleServerErrors);
-app.use(express.json());
 
 module.exports = app;
